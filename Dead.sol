cc96c0516ba903617d6426d8ff2b6f5dbaac9267
christophermontero <chris.to20@hotmail.com>
2020-12-02 12:01:54 -0500

    Dead contract inherits from Ownable

---
+++ Dead.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.4.21;
+
+import "./Ownable.sol";
+
+/**
+ * @title Dead
+ * @dev Kill the contract
+ */
+contract Dead is Ownable{
+
+}
--- Ownable.sol
+++ Ownable.sol
@@ -1,14 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.4.21;
 
-contract Ownable {
-    address owner;
 
+/**
+ * @title Ownable
+ * @dev Set owner
+ */
+contract Ownable {
+    address private owner;
+
+    /**
+     * @dev Set contract deployer as owner
+     */
     function Ownable() public {
         owner = msg.sender;
     }
 
+    // modifier to check if caller is owner
     modifier Owned {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Caller is not owner");
         _;
     }
 }
