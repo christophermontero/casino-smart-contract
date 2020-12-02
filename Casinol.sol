31dec473726b1459f993a7013708795d0612922e
christophermontero <chris.to20@hotmail.com>
2020-12-02 12:44:23 -0500

    Add bet function

--- Casinol.sol
+++ Casinol.sol
@@ -13,12 +13,15 @@ import "./Dead.sol";
      uint minBet;
      uint houseEdge;
      uint winningNum;
+     uint amountWon;
 
     // event for Winner
      event Won(bool status, uint amount);
 
     /**
      * @dev Set the initial state of contract
+     * @param _minBet Minimum bet
+     * @param _houseEdge House profit margin in %
      */
      constructor(uint _minBet, uint _houseEdge) payable{
          require(_minBet > 0);
@@ -27,7 +30,24 @@ import "./Dead.sol";
          houseEdge = _houseEdge;
      }
 
-     function() public {
+     fallback() public {
          revert();
+     }
+
+    /**
+     * @dev Trigger the event when user wons
+     * @param num Number to bet
+     */
+     function bet(uint num) payable public{
+         require(num > 0 && num <= 10);
+         require(msg.value >= minBet);
+         winningNum = (uint256(keccak256(block.timestamp, block.difficulty) % 3) + 1;
+         if (num == winningNum) {
+             uint amountWon = msg.value * (100 - houseEdge)/10;
+             if(!msg.sender.send(amountWon)) revert();
+             emit Won(true, amountWon);
+         } else {
+             emit Won(false, 0);
+         }
      }
  }
